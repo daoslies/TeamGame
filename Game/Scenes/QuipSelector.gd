8338b3372d82245a9eb4e863ae53b665c77b03c7
Naybellez <nn268@sussex.ac.uk>
2023-10-21 19:15:26 +0100

    positioning buttons

--- Game/Scenes/QuipSelector.gd
+++ Game/Scenes/QuipSelector.gd
@@ -2,7 +2,18 @@ extends RichTextLabel
 
 #import character
 
+"""
+	var x = FileAccess.open("res://Filled_Characters/Ghost.json", FileAccess.READ)
+	
+	var data_text = x.get_as_text()
+	var json = JSON.new()
+	var data_parse = json.parse(data_text)
+	var data = json.get_data()
+	var personify = data
+	print('Get Character: ', personify.Name)
+	pass # Replace with function body.
 
+"""
 var seconds = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
